fe794dce48330b35d41996f4ea3fd43a08b3e90c
dcop94 <dcop94@gmail.com>
2024-05-03 12:44:17 +0900

    strtin4 e

--- CP/string4.cpp
+++ CP/string4.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include < string>
 #include <Windows.h>
+#define Name_index 0
+#define Age_index 1
+#define Birth_index 2
 
 using namespace std;
 string notice = "  < 학생 정보 입력 프로그램 사용법 >  \n"
 " * 작성 할 학생 수를 입력 후, 학생 정보를 입력 할 수 있습니다 . \n"
 " * 학생정보 입력은 예시 ) 홍길동 19 0102 의 형식으로 작성하면 됩니다. \n"
-" * 정보 입력이 끝나면 수정 혹은 프로그램을 종료 할 수 있습니다. \n";
+" * 사용자 메뉴의 번호를 입력하여 사용하세요 \n";
 
 
 string** Create_Student_Matrix(int stu_num);
@@ -35,13 +38,50 @@ int main()
     
     string** matrix = Create_Student_Matrix(stu_num);
     Input_Student_Info(matrix, stu_num);
-    Output_Student_Info(matrix, stu_num);
-    Avg_Student_Age(matrix, stu_num);
-    Early_Birth_Student(matrix, stu_num);
-    Modify_Student_Info(matrix, stu_num);
 
+    char choice_func_number;
 
+    do
+    {
+        cout << " " << endl;
+        cout << " < 사용자 메뉴 > " << endl;
+        cout << " 1. 학생 정보 출력하기 " << endl;
+        cout << " 2. 학생 평균 나이 출력하기 " << endl;
+        cout << " 3. 생일이 가장 빠른 학생 출력하기 " << endl;
+        cout << " 4. 학생 정보 수정하기 " << endl;
+        cout << " 5. 프로그램 종료하기 " << endl;
+        cout << " 사용하실 번호를 입력해주세요 ( 1 ~ 5 ) : " << " ";
 
+        cin >> choice_func_number;
+
+        switch (choice_func_number)
+        {
+        case '1':
+            Output_Student_Info(matrix, stu_num);
+            break;
+
+        case '2':
+            Avg_Student_Age(matrix, stu_num);
+            break;
+
+        case '3':
+            Early_Birth_Student(matrix, stu_num);
+            break;
+
+        case '4':
+            Modify_Student_Info(matrix, stu_num);
+            break;
+
+        case '5':
+            cout << " 프로그램을 종료 합니다. " << endl;
+            return 0;
+       
+        default :
+            cout << " 잘못 입력했습니다. 다시 입력하세요 " << endl;
+        }
+    } while (choice_func_number != '5');
+   
+ 
     for (int i = 0; i < stu_num; i++)
     {
         delete[]  matrix[i];
@@ -55,16 +95,16 @@ int main()
 
 string **Create_Student_Matrix(int stu_num) // 입력 받은 학생 수에 맞게 2차원 동적 배열 표를 작성, (이름, 나이, 생일) 카테고리 생성.
 {
-   string **matrix = new string *[stu_num];
+   string **matrix = new string *[stu_num+1];
 
-    for (int i = 0; i < stu_num; i++)
+    for (int i = 0; i < stu_num+1; i++)
     {
         matrix[i] = new string[3];
     }
 
-    matrix[0][0] = "이름";
-    matrix[0][1] = "나이";
-    matrix[0][2] = "생일";
+    matrix[0][Name_index] = "이 름";
+    matrix[0][Age_index] = "나 이";
+    matrix[0][Birth_index] = "생 일";
 
     return matrix;
 }
@@ -74,9 +114,9 @@ void Input_Student_Info(string **matrix, int stu_num) // 카테고리 순서(이름, 나�
 
     cout << " 학생정보 입력 예시 ( 홍길동, 19, 0101 )" << endl;
     
-    for (int i = 0; i < stu_num; i++)
+    for (int i = 1; i < stu_num+1; i++)
     {
-        cout <<  i << "  번 학생 정보를 입력 해주세요 (이름, 나이, 생일) : " << endl;
+        cout << " " << i << " 번 학생 정보를 입력 해주세요 (이름, 나이, 생일) : " << " ";
         cin >> matrix[i][0] >> matrix[i][1] >> matrix[i][2];
     }
 }
@@ -85,12 +125,11 @@ void Output_Student_Info(string **matrix, int stu_num) // 입력 받은 학생 정보 출
 {
     cout << " ---------------------------- " << endl;
     cout << " 작성하신 학생 정보 입니다. " << endl;
-    for (int i = 0; i < stu_num; i++)
+    cout << " " << matrix[0][0] << " | " << matrix[0][1] << " | " << matrix[0][2] << endl; // 카테고리 출력
+    for (int i = 1; i < stu_num+1; i++)
     {
         cout << " " << matrix[i][0] << " | " << matrix[i][1] << " | " << matrix[i][2] << endl;
     }
-   
-
     cout << " ---------------------------- " << endl;
 }
 
@@ -99,13 +138,13 @@ float Avg_Student_Age(string** matrix, int stu_num) // 입력 받은 학생의 나이를 �
     float sum_age = 0;
     float avg_age;
 
-    for (int i = 1; i < stu_num; i++)
+    for (int i = 1; i < stu_num+1; i++)
     {
         sum_age += stoi(matrix[i][1]);
     }
 
     avg_age = sum_age / stu_num;
-    cout << "학생의 평균 나이는 : " << avg_age << " 살 입니다." << endl << endl;
+    cout << endl << " " << "학생의 평균 나이는 : " << avg_age << " 살 입니다." << endl << endl;
     return avg_age;
 }
 
@@ -114,7 +153,7 @@ void Early_Birth_Student(string** matrix, int stu_num) // 입력 받은 학생의 생일 
     string last_birth = "1231";
     int early_birth_index = -1;
     
-    for (int i = 0; i < stu_num; i++)
+    for (int i = 1; i < stu_num+1; i++)
     {
         if (matrix[i][2] < last_birth)
         {
@@ -124,54 +163,37 @@ void Early_Birth_Student(string** matrix, int stu_num) // 입력 받은 학생의 생일 
         }
     }
 
-    cout << "가장 생일이 빠른 학생의 이름과 생일은 : " << matrix[early_birth_index][0] << " / " << matrix[early_birth_index][2] << " 입니다. " << endl << endl;
+    cout << endl << " " << "가장 생일이 빠른 학생의 이름과 생일은 : " << matrix[early_birth_index][0] << " / " << matrix[early_birth_index][2] << " 입니다. " << endl << endl;
 }
 
-void Modify_Student_Info(string** matrix, int stu_num) // 학생정보 수정 및 프로그램 종료 이벤트, 입력을 잘못했을 시 오류처리로도 사용
+void Modify_Student_Info(string** matrix, int stu_num) // 학생정보 수정
 {
-    char choice;
+    int stu_index;
+    bool check_stu_index = false;
+
     do
     {
-        cout << " " << endl;
-        cout << " 어떤 것을 하고 싶으신가요 ? \n ( y : 학생 정보 수정 / n : 프로그램 종료 ) " << endl;
-        cin >> choice;
+        
+        cout << endl << " 수정 할 학생의 번호를 입력해주세요 :  ";
+        cin >> stu_index;
 
-
-        if (choice == 'y')
+        if (stu_index >= 1 && stu_index < stu_num+1)
         {
-            int stu_index;
-            cout << " 수정 할 학생의 번호를 입력해주세요 :  ";
-            cin >> stu_index;
+            cout << endl << " 수정 할 학생의 현재 정보 입니다 \n" << " " << matrix[stu_index][0] << " | " << matrix[stu_index][1] << " | " << matrix[stu_index][2] << endl << endl;
 
-            if (stu_index >= 1 && stu_index < stu_num)
-            {
-                cout << " 수정 할 학생의 현재 정보 입니다 \n" << matrix[stu_index][0] << " | " << matrix[stu_index][1] << " | " << matrix[stu_index][2] << endl << endl;
+            cout << " 수정 할 학생 정보를 입력 해주세요 (이름, 나이, 생일) : " << " ";
+            cin >> matrix[stu_index][0] >> matrix[stu_index][1] >> matrix[stu_index][2];
 
-                cout << " 수정 할 학생 정보를 입력 해주세요 (이름, 나이, 생일) : " << endl;
-                cin >> matrix[stu_index][0] >> matrix[stu_index][1] >> matrix[stu_index][2];
+            cout << endl << " 수정 된 학생 정보 입니다. \n" << " " << matrix[stu_index][0] << " | " << matrix[stu_index][1] << " | " << matrix[stu_index][2] << endl;
 
-                cout << " 수정 된 학생 정보 입니다. \n" << matrix[stu_index][0] << " | " << matrix[stu_index][1] << " | " << matrix[stu_index][2] << endl;
-
-            }
-            else
-            {
-                cout << " 학생 번호가 잘못 입력 되었습니다. 다시 입력 해주세요 : ";
-            }
-
+            break;
         }
-        else if (choice == 'n')
+        else if ( stu_index < 1 || stu_index >= stu_num+1)
         {
-            cout << " 프로그램을 종료하겠습니다. ";
-            exit(0);
+            cout << " 학생 번호가 잘못 입력 되었습니다. 다시 입력 해주세요 : ";
         }
-        else
-        {
-            cout << " 잘못 입력 되었습니다. 다시 입력해주세요 ( y : 학생 정보 수정 / n : 프로그램 종료 )";
-        }
-    } while (choice != 'y' && choice != 'n');
-    
+    } while (true);
 }
-
 
 
 // 1. 사용자에게 몇 명의 학생을 입력할 것인지 묻기
